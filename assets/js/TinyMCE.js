2025a3d98e277bab6fbad7e6008573ca4ea3e079
ммđ αмıи ᴾᴴᴾ <51942680+Mr-Basirnia@users.noreply.github.com>
2022-11-26 14:00:32 +0330

    :sparkles: feat(TinyMCE): add button.
    * Create new option to TinyMCE for paste ipsum text.

---
+++ assets/js/TinyMCE.js
@@ -0,0 +1,30 @@
+(function () {
+
+    tinymce.PluginManager.add('loremipsum', function (editor, url) {
+        editor.addButton('loremipsum', {
+            text: '',
+            image: url + '/../images/icon.png',
+            type: 'button',
+            onclick: function () {
+                editor.windowManager.open({
+                    title: 'Insert Lorem Ipsum Words',
+                    body: [
+                        {
+                            type: 'textbox',
+                            name: 'numWords',
+                            label: 'Number of words to insert?',
+                            value: '130'
+                        }
+                    ],
+                    onsubmit: function (e) {
+                        jQuery.post(url + '/../../includes/TinyMCE/TinyMCEAjax.php?words=' + e.data.numWords, function (data) {
+                            editor.insertContent(data);
+                            editor.windowManager.close();
+                        });
+                    }
+                });
+            }
+        });
+    });
+
+})();
---
+++ includes/TinyMCE/TinyMCE.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace rahgozar\inc\TinyMCE;
+
+class TinyMCE {
+
+
+	public function __construct() {
+		// init process for button control
+		add_action( 'init', array( $this, 'add_button' ) );
+	}
+
+	/**
+	 *
+	 *
+	 * @return void
+	 */
+	public function add_button(): void {
+		// Don't bother doing this stuff if the current user lacks permissions
+		if ( ! current_user_can( 'edit_posts' ) && ! current_user_can( 'edit_pages' ) ) {
+			return;
+		}
+
+		// Add only in Rich Editor mode
+		if ( get_user_option( 'rich_editing' ) == 'true' ) {
+			add_filter( "mce_external_plugins", array( $this, 'ipsum_plugin' ), 0 );
+			add_filter( 'mce_buttons', array( $this, 'register_button' ) );
+		}
+	}
+
+	/**
+	 *
+	 *
+	 * @param $buttons
+	 *
+	 * @return mixed
+	 */
+	public function register_button( $buttons ): mixed {
+		array_push( $buttons, 'separator', 'loremipsum' );
+
+		return $buttons;
+	}
+
+	/**
+	 * Load the TinyMCE plugin : editor_plugin.js (wp2.5)
+	 *
+	 * @param $plugin_array
+	 *
+	 * @return mixed
+	 */
+	public function ipsum_plugin( $plugin_array ): mixed {
+		$plugin_array['loremipsum'] = RAHGOZAR_URL . 'assets/js/TinyMCE.js';
+
+		return $plugin_array;
+	}
+}
