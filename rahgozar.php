6f07b7adea6165e5dce9c273c4f571d5c4a8747d
ммđ αмıи ᴾᴴᴾ <51942680+Mr-Basirnia@users.noreply.github.com>
2022-11-26 11:46:44 +0330

    :sparkles: feat(plugin): build plugin.
    * Create plugin file.

---
+++ rahgozar.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name: Rahgozar
+ * Plugin URI: https://github.com/Mizfa-Tech
+ * Description: Making beautiful words and sentences with Rahgozar.
+ * Version: 1.0.0
+ * Author: Mizfa-Tech
+ * Author URI: https://github.com/Mizfa-Tech
+ * Text Domain: rahgozar
+ * Domain Path: /languages/
+ * Requires at least: 5.8
+ * Requires PHP: 8.0
+ *
+ * @package Rahgozar
+ */
+
+defined( 'ABSPATH' ) || exit;
