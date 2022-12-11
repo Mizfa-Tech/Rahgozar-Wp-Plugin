b8e104b7319f1fcfd88a12d6419385f498917d0c
ммđ αмıи ᴾᴴᴾ <51942680+Mr-Basirnia@users.noreply.github.com>
2022-12-11 15:26:38 +0330

    :pushpin: build(composer): push vendor folder.

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'mr-basirnia/rahgozar',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '597f298252d76a2b96fd8f588037215c202b3437',
+        'type' => 'wp-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'mr-basirnia/rahgozar' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '597f298252d76a2b96fd8f588037215c202b3437',
+            'type' => 'wp-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
